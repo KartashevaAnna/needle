d667b35ad1b546990cbb61f2869daa0f454c0dc3
Anna Kartasheva <annakartashevamail@gmail.com>
2023-01-28 16:26:19 +0300

    before first migrations

--- needle/cross/views.py
+++ needle/cross/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
+
 # Create your views here.
 
+
 def index(request):
-    template = 'cross/index.html'
+    template = "cross/index.html"
     return render(request, template)
+
+
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.cache import cache_page
+from django.db.models import Q
+
+from .forms import KitForm, ProjectForm, ProgressForm
+from .models import User, Progress, Kit
+
+
+def get_pagination(request, post_list):
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return page_obj
+
+
+# @cache_page(20, key_prefix='index_page')
+def index(request):
+    kit_list = Kit.objects.all()
+    page_obj = get_pagination(request, kit_list)
+    context = {
+        "page_obj": page_obj,
+    }
+    return render(request, "cross/index.html", context)
