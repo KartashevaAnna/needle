8ea80d4d9e28f8da60afddd1631dde80fd349bd5
Anna Kartasheva <annakartashevamail@gmail.com>
2023-02-01 23:37:09 +0300

    removed secret key

--- needle/needle/settings.py
+++ needle/needle/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s7-7$am)t(pha#yh@*@f4*#8w*sfmb1t@=86-7)k@1j)^-j@2&"
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
